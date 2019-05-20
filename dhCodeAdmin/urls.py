204ec0ffb4bbdb15744ea8e0990cf96396ba266a
Dariuzcm <dariuzcm@gmail.com>
2019-05-19 22:45:27 -0500

    Añadiendo Index

--- dhCodeAdmin/urls.py
+++ dhCodeAdmin/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from dhCodeAdmin.app.view import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Employees/',include('dhCodeAdmin.app.Employees.urls'))
+    path('Employees/',include('dhCodeAdmin.app.Employees.urls')),
+    path('',home),
 ]
